5666227b4984376dc5bf48fbf9578d9ec3918a82
Douglas-KCA <kevin-dou@outlook.com>
2015-08-04 21:30:04 -0600

    Pantalla Muestra y Estandarizacion Pantalla Profe
    Se creo la pantalla muestra y se agrego la estandarizacion a la pantalla
    del profe

---
+++ Proyecto/Laboratorio/frmEtiqueta.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Laboratorio
+{
+    partial class frmMuestra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMuestra));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblRequerimientos = new System.Windows.Forms.Label();
+            this.lblDescripcionMuestra = new System.Windows.Forms.Label();
+            this.txtRequerimientos = new System.Windows.Forms.TextBox();
+            this.txtDescripcionMuestra = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.btnGuardar);
+            this.panel1.Location = new System.Drawing.Point(12, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(133, 311);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Image = ((System.Drawing.Image)(resources.GetObject("btnGuardar.Image")));
+            this.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuardar.Location = new System.Drawing.Point(12, 42);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(110, 50);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(12, 123);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(110, 50);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // lblRequerimientos
+            // 
+            this.lblRequerimientos.AutoSize = true;
+            this.lblRequerimientos.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequerimientos.Location = new System.Drawing.Point(166, 30);
+            this.lblRequerimientos.Name = "lblRequerimientos";
+            this.lblRequerimientos.Size = new System.Drawing.Size(115, 19);
+            this.lblRequerimientos.TabIndex = 1;
+            this.lblRequerimientos.Text = "Requerimientos:";
+            // 
+            // lblDescripcionMuestra
+            // 
+            this.lblDescripcionMuestra.AutoSize = true;
+            this.lblDescripcionMuestra.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcionMuestra.Location = new System.Drawing.Point(166, 165);
+            this.lblDescripcionMuestra.Name = "lblDescripcionMuestra";
+            this.lblDescripcionMuestra.Size = new System.Drawing.Size(147, 19);
+            this.lblDescripcionMuestra.TabIndex = 2;
+            this.lblDescripcionMuestra.Text = "Descripcion Muestra:";
+            // 
+            // txtRequerimientos
+            // 
+            this.txtRequerimientos.Location = new System.Drawing.Point(170, 57);
+            this.txtRequerimientos.Multiline = true;
+            this.txtRequerimientos.Name = "txtRequerimientos";
+            this.txtRequerimientos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRequerimientos.Size = new System.Drawing.Size(481, 93);
+            this.txtRequerimientos.TabIndex = 3;
+            // 
+            // txtDescripcionMuestra
+            // 
+            this.txtDescripcionMuestra.Location = new System.Drawing.Point(170, 200);
+            this.txtDescripcionMuestra.Multiline = true;
+            this.txtDescripcionMuestra.Name = "txtDescripcionMuestra";
+            this.txtDescripcionMuestra.Size = new System.Drawing.Size(481, 105);
+            this.txtDescripcionMuestra.TabIndex = 4;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(12, 209);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(110, 50);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmMuestra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.txtDescripcionMuestra);
+            this.Controls.Add(this.txtRequerimientos);
+            this.Controls.Add(this.lblDescripcionMuestra);
+            this.Controls.Add(this.lblRequerimientos);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMuestra";
+            this.Text = "Muestra";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblRequerimientos;
+        private System.Windows.Forms.Label lblDescripcionMuestra;
+        private System.Windows.Forms.TextBox txtRequerimientos;
+        private System.Windows.Forms.TextBox txtDescripcionMuestra;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
